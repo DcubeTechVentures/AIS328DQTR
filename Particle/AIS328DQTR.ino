b1265f814b49d18431e4b43c9e9f7c7309d38b51
Yadwinder Singh <ryker1990@gmail.com>
2016-05-04 16:11:21 +0530

    Update AIS328DQTR.ino

--- Particle/AIS328DQTR.ino
+++ Particle/AIS328DQTR.ino
@@ -1,35 +1,43 @@
 // Distributed with a free-will license.
 // Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
-// A3G4250DQTR
-// This code is designed to work with the A3G4250DQTR_I2CS I2C Mini Module available from ControlEverything.com.
+// AIS328DQTR
+// This code is designed to work with the AIS328DQTR_I2CS I2C Mini Module available from ControlEverything.com.
 // https://www.controleverything.com/content/Accelorometer?sku=AIS328DQTR_I2CS#tabs-0-product_tabset-2
 
-#include<Wire.h>
+#include <application.h>
+#include <spark_wiring_i2c.h>
 
 // AIS328DQTR I2C address is 0x18(24) 
 #define Addr 0x18
 
+int xAccl = 0, yAccl =  0, zAccl = 0;
 void setup()
 {
-  // Initialise I2C communication as MASTER
+  // Set variable
+  Particle.variable("i2cdevice","AIS328DQTR");
+	Particle.variable("xAccl",xAccl);
+	Particle.variable("yAccl",yAccl);
+	Particle.variable("zAccl",zAccl);
+	
+  // Initialise I2C communication as MASTER 
   Wire.begin();
-  // Initialise Serial Communication, set baud rate = 9600
+  // Initialise serial communication, set baud rate = 9600
   Serial.begin(9600);
-
+    
   // Start I2C Transmission
   Wire.beginTransmission(Addr);
   // Select control register1
   Wire.write(0x20);
-  // Select 50Hz, power on mode, X, Y, Z-Axis enabled  
+  // Select 50Hz, normal power mode  
   Wire.write(0x27);
   // Stop I2C Transmission
   Wire.endTransmission();
-
+    
   // Start I2C Transmission
   Wire.beginTransmission(Addr);
   // Select control register4
   Wire.write(0x23);
-  // Select continuous update, full-scale range +/- 8g
+  // Select full-scale range 
   Wire.write(0x30);
   // Stop I2C Transmission
   Wire.endTransmission();
@@ -44,32 +52,43 @@ void loop()
     // Start I2C Transmission
     Wire.beginTransmission(Addr);
     // Select data register
-    Wire.write((40+i));
+    Wire.write((40 + i));
     // Stop I2C Transmission
     Wire.endTransmission();
-
+    
     // Request 1 byte of data
-    Wire.requestFrom(Addr,1);
-
-    // Read 6 bytes of data
+    Wire.requestFrom(Addr, 1);
+    
+    // Read 1 byte of data
     // xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
-    if(Wire.available()==1)
+    if(Wire.available() == 1)
     {
-      data[i] = Wire.read();
+        data[i] = Wire.read();
     }
     delay(300);
   }
-  
+
   // Convert the data
-  int xAccl = ((data[1] * 256) + data[0]);
-  int yAccl = ((data[3] * 256) + data[2]);
-  int zAccl = ((data[5] * 256) + data[4]);
-  
-  // Output data to serial monitor
-  Serial.print("Acceleration in X-Axis : ");
-  Serial.println(xAccl);
-  Serial.print("Acceleration in Y-Axis : ");
-  Serial.println(yAccl);
-  Serial.print("Acceleration in Z-Axis : ");
-  Serial.println(zAccl);
+  xAccl = (((data[1] & 0xFF) * 256) + (data[0] & 0xFF));
+  if(xAccl > 32767)
+	{
+		xAccl -= 65536;
+	}
+	
+  yAccl = (((data[3] & 0xFF) * 256) + (data[2] & 0xFF));
+  if(yAccl > 32767)
+	{
+		yAccl -= 65536;
+	}
+    
+  zAccl = (((data[5] & 0xFF) * 256) + (data[4] & 0xFF));
+  if(zAccl > 32767)
+	{
+		zAccl -= 65536;
+	}
+
+  // Output data to dashboard
+	Particle.publish("Acceleration in X-Axis :", String(xAccl));
+	Particle.publish("Acceleration in Y-Axis :", String(yAccl));
+	Particle.publish("Acceleration in Z-Axis :", String(zAccl));
 }
