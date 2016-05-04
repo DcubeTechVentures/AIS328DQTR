c3d7d33fd4ada827b1467cf407ec2ab3249ed802
Yadwinder Singh <ryker1990@gmail.com>
2016-05-04 16:12:59 +0530

    Update AIS328DQTR.ino

--- Particle/AIS328DQTR.ino
+++ Particle/AIS328DQTR.ino
@@ -7,37 +7,37 @@
 #include <application.h>
 #include <spark_wiring_i2c.h>
 
-// AIS328DQTR I2C address is 0x18(24) 
+// AIS328DQTR I2C address is 0x18(24)
 #define Addr 0x18
 
 int xAccl = 0, yAccl =  0, zAccl = 0;
 void setup()
 {
   // Set variable
-  Particle.variable("i2cdevice","AIS328DQTR");
-	Particle.variable("xAccl",xAccl);
-	Particle.variable("yAccl",yAccl);
-	Particle.variable("zAccl",zAccl);
-	
-  // Initialise I2C communication as MASTER 
+  Particle.variable("i2cdevice", "AIS328DQTR");
+  Particle.variable("xAccl", xAccl);
+  Particle.variable("yAccl", yAccl);
+  Particle.variable("zAccl", zAccl);
+
+  // Initialise I2C communication as MASTER
   Wire.begin();
   // Initialise serial communication, set baud rate = 9600
   Serial.begin(9600);
-    
+
   // Start I2C Transmission
   Wire.beginTransmission(Addr);
   // Select control register1
   Wire.write(0x20);
-  // Select 50Hz, normal power mode  
+  // Select 50Hz, normal power mode
   Wire.write(0x27);
   // Stop I2C Transmission
   Wire.endTransmission();
-    
+
   // Start I2C Transmission
   Wire.beginTransmission(Addr);
   // Select control register4
   Wire.write(0x23);
-  // Select full-scale range 
+  // Select full-scale range
   Wire.write(0x30);
   // Stop I2C Transmission
   Wire.endTransmission();
@@ -47,7 +47,7 @@ void setup()
 void loop()
 {
   unsigned int data[6];
-  for(int i = 0; i < 6; i++)
+  for (int i = 0; i < 6; i++)
   {
     // Start I2C Transmission
     Wire.beginTransmission(Addr);
@@ -55,40 +55,40 @@ void loop()
     Wire.write((40 + i));
     // Stop I2C Transmission
     Wire.endTransmission();
-    
+
     // Request 1 byte of data
     Wire.requestFrom(Addr, 1);
-    
-    // Read 1 byte of data
+
+    // Read 6 bytes of data
     // xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
-    if(Wire.available() == 1)
+    if (Wire.available() == 1)
     {
-        data[i] = Wire.read();
+      data[i] = Wire.read();
     }
     delay(300);
   }
 
   // Convert the data
   xAccl = (((data[1] & 0xFF) * 256) + (data[0] & 0xFF));
-  if(xAccl > 32767)
-	{
-		xAccl -= 65536;
-	}
-	
+  if (xAccl > 32767)
+  {
+    xAccl -= 65536;
+  }
+
   yAccl = (((data[3] & 0xFF) * 256) + (data[2] & 0xFF));
-  if(yAccl > 32767)
-	{
-		yAccl -= 65536;
-	}
-    
+  if (yAccl > 32767)
+  {
+    yAccl -= 65536;
+  }
+
   zAccl = (((data[5] & 0xFF) * 256) + (data[4] & 0xFF));
-  if(zAccl > 32767)
-	{
-		zAccl -= 65536;
-	}
+  if (zAccl > 32767)
+  {
+    zAccl -= 65536;
+  }
 
   // Output data to dashboard
-	Particle.publish("Acceleration in X-Axis :", String(xAccl));
-	Particle.publish("Acceleration in Y-Axis :", String(yAccl));
-	Particle.publish("Acceleration in Z-Axis :", String(zAccl));
+  Particle.publish("Acceleration in X-Axis is :", String(xAccl));
+  Particle.publish("Acceleration in Y-Axis is :", String(yAccl));
+  Particle.publish("Acceleration in Z-Axis is :", String(zAccl));
 }
